12f5ec2466b8b1ccdb37a082cad7145e014fb6d2
18ei8i9 <nickenbote@gmail.com>
2024-08-05 08:29:26 -0300

    JUEGAN OTRAVEZ O REINICIAN

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const impar = [1,3,5,7,9,11];
 const baja = [1,2,3,4,5,6];
 const alta = [7,8,9,10,11,12];
 let jugadores=[];
+let jugadoresiniciales=[];
 let turno=0;
 let elecciones=[];
 let ganadores=[];
@@ -26,9 +27,8 @@ function asignarTextoElemento(elemento, texto) {
 function condicionesIniciales() {
     asignarTextoElemento('h1','GUERRA O COSA');
     asignarTextoElemento('p',`INGRESE JUGADORES`);
-    fase = 1;
-    carta=elegirValores(palo,numero)
-    console.log(carta);
+    let jugadoresiniciales=[];
+
     document.getElementById('nombre').style.display="block";
     document.getElementById('agregar').style.display="block";
     document.getElementById('terminar').style.display="block";
@@ -56,6 +56,7 @@ function condicionesIniciales() {
     document.getElementById('12').style.display="none";
     document.getElementById('continuar').style.display="none";
     document.getElementById('reiniciar').style.display="none";
+    document.getElementById('jugar').style.display="none";
     
 }
 
@@ -63,9 +64,9 @@ function condicionesIniciales() {
 function agregaJugador(){
     let nombre = document.getElementById("nombre").value;
     if(nombre !==""){
-        jugadores.push(nombre);
+        jugadoresiniciales.push(nombre);
         document.getElementById('nombre').value = '';
-        console.log(jugadores);
+        console.log(jugadoresiniciales);
     }
 }
 
@@ -76,7 +77,12 @@ function terminaCarga(){
     document.getElementById('terminar').style.display="none";
     document.getElementById('guerra').style.display="block";
     document.getElementById('cosa').style.display="block";
-    
+    document.getElementById('reiniciar').style.display="none";
+    document.getElementById('jugar').style.display="none";
+    jugadores=jugadoresiniciales;
+    fase = 1;
+    carta=elegirValores(palo,numero);
+    console.log(carta);
     console.log(jugadores);
     
     asignarTextoElemento('p',`jugador ${jugadores[turno]} elije una opcion`)
@@ -208,6 +214,7 @@ function compararConArrayAnterior(arrayActual) {
                 }
                 document.getElementById('reiniciar').style.display="block";
                 asignarTextoElemento('p',`NO ADIVINO NADIE`);
+                document.getElementById('jugar').style.display="block";
                 jugadores=[];
             }
         
@@ -222,67 +229,6 @@ function compararConArrayAnterior(arrayActual) {
         asignarTextoElemento('p',`jugador ${jugadores[turno]} elije una opcion`);
     }
  }
-    /*else if (tienenValoresIguales(carta, arrayActual) & fase==1 ){
-        document.getElementById('guerra').style.display="none";
-        document.getElementById('cosa').style.display="none";
-        if(tienenValoresIguales(guerra, arrayActual)){
-            asignarTextoElemento('p',`ELIGE ESPADA O BASTO`);
-        document.getElementById('espada').style.display="block";
-        document.getElementById('basto').style.display="block";}
-        else if(tienenValoresIguales(cosa, arrayActual)){
-            asignarTextoElemento('p',`ELIGE ORO O COPAS`);
-        document.getElementById('oro').style.display="block";
-        document.getElementById('copa').style.display="block";}
-        fase++;
-        console.log(fase);
-
-    }else if(tienenValoresIguales(carta, arrayActual) & fase==3 ){
-        asignarTextoElemento('p',`ES UNA CARTA BAJA (1 AL 6) O ALTA (7 AL 12)?`);
-        document.getElementById('alta').style.display="block";
-        document.getElementById('baja').style.display="block";
-        document.getElementById('par').style.display="none";
-        document.getElementById('impar').style.display="none";
-        console.log('Array anterior:', carta);
-        fase++;
-        console.log(fase);
-
-    }else if(tienenValoresIguales(carta, arrayActual) & fase==4 ){
-        asignarTextoElemento('p',`DALE CRACK ARRIESGA QUE YA ES TUYO!!!`);
-        document.getElementById('alta').style.display="none";
-        document.getElementById('baja').style.display="none";
-        console.log('Array anterior:', carta);
-        fase++;
-        console.log(fase);
-        if(tienenValoresIguales(carta, par) & tienenValoresIguales(carta, baja)){
-            document.getElementById('2').style.display="block";
-            document.getElementById('4').style.display="block";
-            document.getElementById('6').style.display="block";
-            
-            
-        }else if(tienenValoresIguales(carta, par) & tienenValoresIguales(carta, alta)){
-            document.getElementById('8').style.display="block";
-            document.getElementById('10').style.display="block";
-            document.getElementById('12').style.display="block";
-
-        }else if(tienenValoresIguales(carta, impar) & tienenValoresIguales(carta, baja)){
-            document.getElementById('1').style.display="block";
-            document.getElementById('3').style.display="block";
-            document.getElementById('5').style.display="block";
-
-
-        }else{
-            document.getElementById('7').style.display="block";
-            document.getElementById('9').style.display="block";
-            document.getElementById('11').style.display="block";
-
-        }
-
-    }else{
-        alert("PERDISTE");
-        condicionesIniciales();
-        
-    }
-}*/
 
 function compararConValor (valorPalo){
     elecciones[turno] = fase==5 ? buscaValor(carta,parseInt(valorPalo)):buscaValor(carta,valorPalo);
@@ -291,44 +237,55 @@ function compararConValor (valorPalo){
     }
     console.log(turno);
     console.log(elecciones);
-    if(ganadores.length!=0){
-        if(turno==jugadores.length-1){
-           document.getElementById('copa').style.display="none";
-           document.getElementById('oro').style.display="none";
-           document.getElementById('espada').style.display="none";
-           document.getElementById('basto').style.display="none";
-           document.getElementById('1').style.display="none";
-           document.getElementById('2').style.display="none";
-           document.getElementById('3').style.display="none";
-           document.getElementById('4').style.display="none";
-           document.getElementById('5').style.display="none";
-           document.getElementById('6').style.display="none";
-           document.getElementById('7').style.display="none";
-           document.getElementById('8').style.display="none";
-           document.getElementById('9').style.display="none";
-           document.getElementById('10').style.display="none";
-           document.getElementById('11').style.display="none";
-           document.getElementById('12').style.display="none";
-           document.getElementById("h3").innerHTML=" ";
-           for(i=0;i<ganadores.length;i++){
-
+    if(turno==jugadores.length-1){
+        document.getElementById('copa').style.display="none";
+        document.getElementById('oro').style.display="none";
+        document.getElementById('espada').style.display="none";
+        document.getElementById('basto').style.display="none";
+        document.getElementById('1').style.display="none";
+        document.getElementById('2').style.display="none";
+        document.getElementById('3').style.display="none";
+        document.getElementById('4').style.display="none";
+        document.getElementById('5').style.display="none";
+        document.getElementById('6').style.display="none";
+        document.getElementById('7').style.display="none";
+        document.getElementById('8').style.display="none";
+        document.getElementById('9').style.display="none";
+        document.getElementById('10').style.display="none";
+        document.getElementById('11').style.display="none";
+        document.getElementById('12').style.display="none";
+        document.getElementById("h3").innerHTML=" ";
+        if(ganadores.length!=0){
+            for(i=0;i<ganadores.length;i++){
                document.getElementById("h3").innerHTML +=  ganadores[i] + "<br>" ;
-           }
-           if(fase==5){
+            }
+            if(fase==5){
                asignarTextoElemento('p',`DALE CAMPEÓN, DALE CAMPEÓN!!!`);
                document.getElementById('reiniciar').style.display="block";
-          }
-          else{
-            document.getElementById('continuar').style.display="block";
-            asignarTextoElemento('p',`ADIVINARON Y PASAN DE RONDA`);
-            jugadores=ganadores;
-            turno=0;
-            ganadores=[];
-            elecciones=[];
-            fase++;
-          }
+               document.getElementById('jugar').style.display="block";
+               jugadores=[];
+               }
+            else{
+                document.getElementById('continuar').style.display="block";
+                asignarTextoElemento('p',`ADIVINARON Y PASAN DE RONDA`);
+                jugadores=ganadores;
+                turno=0;
+                ganadores=[];
+                elecciones=[];
+                fase++;
+            }
         }
+        else{
+            for(i=0;i<carta.length;i++){
+
+                document.getElementById("h3").innerHTML +=  carta[i] + "<br>" ;
+            }
+            document.getElementById('reiniciar').style.display="block";
+            asignarTextoElemento('p',`NO ADIVINO NADIE`);
+            document.getElementById('jugar').style.display="block";
+            jugadores=[];
         }
+    }
     else {
         console.log(turno);
         
@@ -337,25 +294,6 @@ function compararConValor (valorPalo){
         console.log(ganadores);
         asignarTextoElemento('p',`jugador ${jugadores[turno]} elije una opcion`);
     }
-   /*if(buscaValor(carta,valorPalo) & fase==2){
-        asignarTextoElemento('p',`ES PAR O IMPAR`);
-        document.getElementById('espada').style.display="none";
-        document.getElementById('basto').style.display="none";
-        document.getElementById('copa').style.display="none";
-        document.getElementById('oro').style.display="none";
-        document.getElementById('par').style.display="block";
-        document.getElementById('impar').style.display="block";
-        console.log('Array anterior:', carta);
-        fase++;
-        console.log(fase);
-        }
-    else if(buscaValor(carta,parseInt(valorPalo)) & fase==5 ){
-        console.log("ganaste");
-        alert("VAAAAAAAAMOOOOOOO CAPOOOOOOO");
-    }else{
-        alert("PERDSTE GATO");
-        condicionesIniciales();
-    }*/
 }
 
 
